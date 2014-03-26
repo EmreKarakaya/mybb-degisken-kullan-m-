5159ed1dddb575ee2a625d563b2b9ab5f62ce770
Emre Karakaya <emrekarakaya@yandex.com>
2014-03-26 17:40:38 +0200

    Adminin Girebileceği sayfa eklendi.
    Koddaki hata düzeltildi.

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 define ("IN_MYBB", 1);
 require "global.php";
-if($mybb->user['username'] == "4" )
+if($mybb->user['displaygroup'] == "4" )
 {
 echo $mybb->user['username'];
 }
